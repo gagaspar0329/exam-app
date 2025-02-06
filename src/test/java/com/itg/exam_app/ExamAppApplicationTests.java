e28fcbd5e3da830d11432396fd390932773079d2
gagaspar0329 <glennadriangaspar@gmail.com>
2025-02-07 00:34:24 +0800

    initial commit

---
+++ src/test/java/com/itg/exam_app/ExamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itg.exam_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
