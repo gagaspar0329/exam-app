b0ce878a233d6cab47df89f8ed9e19999b715549
gagaspar0329 <glennadriangaspar@gmail.com>
2025-02-08 06:53:23 +0800

    [new] - added class for accounts attributes

---
+++ src/main/java/com/itg/exam_app/entity/Accounts.java
@@ -0,0 +1,30 @@
+package com.itg.exam_app.entity;
+
+public class Accounts {
+
+	public Long accountNumber;
+	public String accountType;
+	public double availableBalance;
+	
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public double getAvailableBalance() {
+		return availableBalance;
+	}
+	public void setAvailableBalance(double availableBalance) {
+		this.availableBalance = availableBalance;
+	}
+	
+	
+	
+}
