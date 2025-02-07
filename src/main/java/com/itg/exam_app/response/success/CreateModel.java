d3ad4297848270e5aa8649ce378ece87e1aabfab
gagaspar0329 <glennadriangaspar@gmail.com>
2025-02-08 06:53:57 +0800

    [new] - class for customer creation response

---
+++ src/main/java/com/itg/exam_app/response/success/CreateModel.java
@@ -0,0 +1,38 @@
+package com.itg.exam_app.response.success;
+
+import com.itg.exam_app.entity.CustomerEntity;
+
+public class CreateModel {
+	
+	private Long customerNumber;
+	private int transactionStatusCode;
+	private String transactionStatusDescription;
+	
+	public CreateModel(Long customerNumber, int statusCode, String statusDesc) {
+		this.customerNumber = customerNumber;
+		this.transactionStatusCode = statusCode;
+		this.transactionStatusDescription = statusDesc;
+		
+	} 
+	
+	public Long getCustomerNumber() {
+		return customerNumber;
+	}
+	public void setCustomerNumber(Long customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	public int getTransactionStatusCode() {
+		return transactionStatusCode;
+	}
+	public void setTransactionStatusCode(int transactionStatusCode) {
+		this.transactionStatusCode = transactionStatusCode;
+	}
+	public String getTransactionStatusDescription() {
+		return transactionStatusDescription;
+	}
+	public void setTransactionStatusDescription(String transactionStatusDescription) {
+		this.transactionStatusDescription = transactionStatusDescription;
+	}
+	
+	
+}
